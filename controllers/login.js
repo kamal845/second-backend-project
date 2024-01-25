b0cd0e1c6012e2cc3086ffe786303ce96a3e663e
kamal <kk5738547@gmail.com>
2024-01-25 23:12:12 +0530

    logicandroutes.

--- controllers/login.js
+++ controllers/login.js
@@ -1,13 +1,57 @@
 const express=require("express");
+const bcrypt=require("bcrypt");
 const __res=require("../help/response");
 const loginModel=require("../models/login-model");
 // const jwt = require("jwt");
 module.exports={
-    login:async(req,res)=>{
+    securePassword:async(password)=>{
+try {
+    const passwordHashed = await bcrypt.hash(password, 10);
+  return  passwordHashed;
+} catch (error) {
+    return __res.out(req, res, {
+        statusCode: 400,
+        message: "not done",
+      });
+}
+    },
+
+    login:async(req,res)=>{  
     try {
-        
+        const email=req.body.Email;
+        const password=req.body.Password;
+        const username=req.body.Username;
+        const logedin=await loginModel.find({email:Email});
+        if (logedin) {
+            //ye password ko compare karne ke liye hai.
+         const passwordMatch= await bcrypt.compare(password,logedin.Password);
+         if(passwordMatch){
+            const logedResult={
+                name:logedin.Username,
+                email:logedin.Email,
+                password:logedin.Password
+            }
+         }
+         const response={
+            msg:"loged in",
+            data:logedin
+         }
+         return __res.out(req, res, {
+            statusCode: 200,
+            message: "success",
+            data: response,
+          });
+        } else {
+            return __res.out(req, res, {
+                statusCode: 404,
+                message: "invaled credential",
+              });  
+        }
     } catch (error) {
-        
+        return __res.out(req, res, {
+            statusCode: 500,
+            message: "internal server error",
+          });
     }
     },
     logout:async(req,res)=>{
--- models/login-model.js
+++ models/login-model.js
@@ -1,13 +1,18 @@
-const mongoose=require("mongoose");
-const loginschema=mongoose.Schema({
-Username:{
-    type:String,
-    required:true
-},
-password:{
-    type:String,
-    required:true,
-    minlength:3
-}
-})
-module.exports=mongoose.model('login',loginschema);
+const mongoose = require("mongoose");
+const loginschema = mongoose.Schema({
+  Username: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Password: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+});
+module.exports = mongoose.model("login", loginschema);
